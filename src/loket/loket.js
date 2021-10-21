62e52e7bdd7b1318ba4a69db1c176c9fca8659e1
Deddy Setiawan <deddysetiawan17@gmail.com>
2021-10-21 23:12:33 +0700

    [FEATURE] able to fecth with axios

--- src/loket/loket.js
+++ src/loket/loket.js
@@ -1,17 +1,21 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
 import "./loket.css";
+import CountUp from 'react-countup';
+import VisibilitySensor from 'react-visibility-sensor';
 
 export function Loket() {
-  const semuaLoket = {
-    total : 4,
+  const semuaLokets = {
+    total : 0,
     isi :[
       {
         loket: "Loket 1",
         nama: "Bidang Kemahasiswaan",
-        total: 1,
+        jumlah: 0,
         isi: [
           {
             nama: "Konsul Laporan Beasiswa",
-            jumlah: 1,
+            jumlah: 0,
           },
           {
             nama: "Buka Blokir Rekening",
@@ -30,11 +34,11 @@ export function Loket() {
       {
         loket: "loket 2",
         nama: "Bidang Akademik",
-        total: 3,
+        jumlah: 0,
         isi: [
           {
             nama: "Bebas Akademik",
-            jumlah: 2,
+            jumlah: 0,
           },
           {
             nama: "Pengambilan Ijazah",
@@ -46,12 +50,20 @@ export function Loket() {
           },
           {
             nama: "Pengambilan KRS",
-            jumlah: 1,
+            jumlah: 0,
           },
         ],
       },
     ]
   };
+
+  const [semuaLoket, setSemuaLoket] = useState(semuaLokets)
+
+  useEffect(() => {
+    axios
+      .get("https://antrian-api.herokuapp.com/loket")
+      .then(response => setSemuaLoket(response.data));
+  }, []);
 
   return (
     <div className="container">
@@ -66,7 +78,15 @@ export function Loket() {
         <div className="container mt-2">
           <div className="row px-1 px-lg-0">
             <div className="col-lg-6 bg-login bg-light text-dark mx-auto rounded-2 all-center flex-column">
-              <h1 className="font-counter mt-2 mt-lg-0 fs-1">{semuaLoket.total}</h1>
+              <h1 className="font-counter mt-2 mt-lg-0 fs-1">
+                <CountUp end={semuaLoket.total} duration={1} redraw={true}>
+                  {({ countUpRef, start }) => (
+                    <VisibilitySensor onChange={start} delayedCall>
+                        <span ref={countUpRef} />
+                    </VisibilitySensor>
+                  )}
+                </CountUp>
+              </h1>
               <h6 className="fw-light mb-4 mb-lg-2 fs-6">JUMLAH ANTRIAN</h6>
             </div>
           </div>
@@ -78,7 +98,15 @@ export function Loket() {
                 <div className="col-lg-3 bg-login bg-light text-dark rounded-2 all-center flex-column mx-lg-3 my-2">
                   <div className="border-2 border-bottom w-100 text-center mb-2">
                     <h6 className="fw-bold fs-5 mt-2 text-uppercase">{loket.loket}</h6>
-                    <h6 className="fw-light fs-3">{loket.total}</h6>
+                    <h6 className="fw-light fs-3">
+                      <CountUp end={loket.jumlah} duration={1} redraw={true}>
+                        {({ countUpRef, start }) => (
+                          <VisibilitySensor onChange={start} delayedCall>
+                              <span ref={countUpRef} />
+                          </VisibilitySensor>
+                        )}
+                      </CountUp>
+                    </h6>
                     <h6 className="fw-normal fs-6 mt-2 text-uppercase">{loket.nama}</h6>
                   </div>
                   {loket.isi.map((isi) => {
@@ -87,7 +115,15 @@ export function Loket() {
                         <dt className="col-10 d-flex align-items-center text-start border-light border-2 border-end font-sub-loket">
                           {isi.nama}
                         </dt>
-                        <dd className="col-2 pt-2 all-center">{isi.jumlah}</dd>
+                        <dd className="col-2 pt-2 all-center">
+                          <CountUp end={isi.jumlah} duration={1} redraw={true}>
+                            {({ countUpRef, start }) => (
+                              <VisibilitySensor onChange={start} delayedCall>
+                                  <span ref={countUpRef} />
+                              </VisibilitySensor>
+                            )}
+                          </CountUp>
+                          </dd>
                       </dl>
                     )
                   })}
